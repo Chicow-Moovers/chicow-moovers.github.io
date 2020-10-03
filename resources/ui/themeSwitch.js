e34601037976355ee52b6cfffbaca6efe4d2fdcd
Henry Heino <henryuheino@gmail.com>
2020-10-03 01:42:27 -0700

    Save theme settings

--- resources/ui/themeSwitch.js
+++ resources/ui/themeSwitch.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Suggests /LibJS/Libs/JSHelper.js
+// Requires /LibJS/Libs/StorageHelper.js
 
 (async function()
 {
@@ -9,18 +10,20 @@
     const themeSwitch = document.querySelector("button#changeTheme");
     const root = document.querySelector(":root");
 
+    root.setAttribute("data-theme", StorageHelper.get("data-theme"));
+
     themeSwitch.addEventListener("click", async () =>
     {
         const currentTheme = root.getAttribute("data-theme");
+        let newTheme = "light";
 
-        if (currentTheme == "dark")
+        if (currentTheme != "dark")
         {
-            root.setAttribute("data-theme", "light");
+            newTheme = "dark";
         }
-        else
-        {
-            root.setAttribute("data-theme", "dark");
-        }
+
+        root.setAttribute("data-theme", newTheme);
+        StorageHelper.put("data-theme", newTheme);
 
         // See https://medium.com/@mwichary/dark-theme-in-a-day-3518dde2955a
         root.classList.add("transitioning-color-theme");
