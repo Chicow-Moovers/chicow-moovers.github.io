76e48c19cc603518abfaf0e20ac54d3a79b83a03
Henry Heino <henryuheino@gmail.com>
2020-10-03 23:01:44 -0700

    Support for multiple rows of tiles

--- resources/js/propulsionStats.js
+++ resources/js/propulsionStats.js
@@ -3,8 +3,11 @@
 // Note: themeSwitch.js is rather over-commented. It may be helpful,
 // it may not be...
 
+// Function forward-declarations.
+let handleTileGroup;
+
 // We use async here so we can use await inside this function...
-(async function()
+const main = async function()
 {
     // Wait for page load...
     await JSHelper.Notifier.waitFor(JSHelper.GlobalEvents.PAGE_SETUP_COMPLETE);
@@ -13,6 +16,10 @@
     const statArea = document.querySelector(".propulsionStats");
     const canvas = document.querySelector(".propulsionStats canvas");
     const tiles = document.querySelectorAll(".propulsionStats .tiles .tile");
+
+    const propulsionOptions = handleTileGroup("propulsionChoice");
+    const planetOption = handleTileGroup("planetChoice");
+    const objectOption = handleTileGroup("objectChoice");
 
     // Display the current status...
     const updateCtxLoop = 
@@ -47,30 +54,92 @@
         }
     });
 
-    updateCtxLoop(); // Loop, loop, loop. But lets us run everything below here first.
 
-    let activeTile = null; // Initially, no active tile.
+    updateCtxLoop(); // Loop, loop, loop. But lets us run everything below here first.
+};
+
+///  Handle a single group of .tiles
+/// This group should have classes `.tiles` and
+/// [groupClassNS].
+///  Allow users to select one in a group
+/// of tiles.
+///
+/// Returns an object with methods:
+///     getCurrentTileName() ----> Returns the "name"
+///         attribute of the current tile.
+///     setSelectedTile(name) ----> Select the tile with attr name=[name].
+handleTileGroup = (groupClassNS) =>
+{
+    // Note: `.tiles${groupClassNS} .tile` is equivalent to ".tiles" + groupClassNS + " .tile".
+    const tiles = document.querySelectorAll(`.tiles.${groupClassNS} .tile`);
+    let tilesByName = {};
+
+    let activeTilename = null;
+
+    const handleTile = (tileElem) =>
+    {
+        const name = tileElem.getAttribute("name");
+
+        if (!name)
+        {
+            throw new Error("All .tile elements must have a name attribute!" +
+            " Consider setting name='something' on element: " + tileElem.outerHTML);
+        }
+
+        tilesByName[name] = tileElem;
+
+        tileElem.addEventListener("click", () =>
+        {
+            if (name == activeTilename)
+            {
+                activeTilename = null;
+                tileElem.classList.remove("selected");
+            }
+            else
+            {
+                if (activeTilename in tilesByName)
+                {
+                    let activeTile = tilesByName[activeTilename];
+
+                    activeTile.classList.remove("selected");
+                }
+
+                tileElem.classList.add("selected");
+                activeTilename = name;
+            }
+        });
+    };
 
     // Allow tiles to be selected/unselected.
     for (const tile of tiles)
     {
-        tile.addEventListener("click", () =>
+        handleTile(tile);
+    }
+
+    const result =
+    {
+        "getCurrentTileName": () =>
         {
-            if (tile == activeTile)
+            return activeTilename;
+        },
+
+        "setSelectedTile": (name) =>
+        {
+            if (name in tilesByName)
             {
-                activeTile = null;
-                tile.classList.remove("selected");
+                activeTilename = name;
             }
             else
             {
-                if (activeTile)
-                {
-                    activeTile.classList.remove("selected");
-                }
-
-                tile.classList.add("selected");
-                activeTile = tile;
+                activeTilename = null;
             }
-        });
-    }
-})();
+        },
+    };
+
+
+
+    return result;
+};
+
+
+main();
